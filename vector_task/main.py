4bc610a67ab0bafef9c416a390c5ccac545b2701
Galina <galinatankova2703@gmail.com>
2023-09-26 23:13:30 +0700

    1. Methods fixed

--- vector_task/main.py
+++ vector_task/main.py
@@ -1,5 +1,6 @@
 from vector_task.vector import Vector
 
+vector = Vector(3, [1, 2, 3, 4, 5])
 vector_1 = Vector(4, [1, 2, 3])
 vector_2 = Vector(8)
 vector_3 = Vector(vector_1)
@@ -17,10 +18,14 @@ if vector_1 == vector_3:
 if vector_1.size < vector_2.size:
     print("Vector 1 has less number of components than vector 2")
 
-vector_3 = vector_3 * (1 / vector_3.norm)
+vector_3 *= (1 / vector_3.norm)
 
-print("Norm of vector 3", vector_3.norm)
+for i in range(vector_2.size):
+    vector_2[i] = i
+
+print("Norm of vector 3:", vector_3.norm)
 print("Sum of two vectors:", vectors_sum)
 print("Subtraction of two vectors:", vectors_sub)
 print("Turned vector 5:", vector_5)
-print("Dot product:", Vector([1, 1, 1, 1]).dot_product(Vector([2, 2])))
+print("Dot product:", Vector([1, 1, 1, 1]).get_dot_product(Vector([2, 2])))
+print("Changed vector 2:", vector_2)
--- vector_task/vector.py
+++ vector_task/vector.py
@@ -3,30 +3,47 @@ from math import sqrt
 
 class Vector:
     def __init__(self, *args):
-        if len(args) == 1 and isinstance(args[0], int):
-            if args[0] <= 0:
-                raise ValueError(f"Vector size n must be > 0. Your input is n = {args[0]}")
+        if len(args) > 2 or len(args) == 0:
+            raise ValueError("Invalid number of arguments")
 
-            self.__components = [0] * args[0]
+        if len(args) == 1 and isinstance(args[0], int):
+            n = args[0]
+
+            if n <= 0:
+                raise ValueError(f"Vector size must be > 0. Your input is {n}")
+
+            self.__components = [0] * n
 
         elif len(args) == 1 and isinstance(args[0], Vector):
-            self.__components = list(args[0].__components)
+            vector = args[0]
+            self.__components = list(vector.__components)
 
         elif len(args) == 1 and isinstance(args[0], list):
-            if len(args[0]) == 0:
-                raise ValueError(f"Vector size n must be > 0. Your input is n = {args[0]}")
+            components_list = args[0]
 
-            self.__components = list(args[0])
+            if len(components_list) == 0:
+                raise ValueError(f"Vector size must be > 0. Your input is {components_list}")
+
+            if not all(isinstance(item, (int, float)) for item in components_list):
+                raise TypeError("Vector components must be numbers")
+
+            self.__components = list(components_list)
 
         elif len(args) == 2 and isinstance(args[0], int) and isinstance(args[1], list):
+            n = args[0]
+            components_list = args[1]
 
-            if args[0] <= 0:
-                raise ValueError(f"Vector size n must be > 0. Your input is n = {args[0]}")
+            if n <= 0:
+                raise ValueError(f"Vector size must be > 0. Your input is {n}")
 
-            if len(args[1]) < args[0]:
-                self.__components = args[1] + [0] * (args[0] - len(args[1]))
+            if not all(isinstance(item, (int, float)) for item in components_list):
+                raise TypeError("Vector components must be numbers")
+
+            if len(components_list) >= n:
+                self.__components = components_list[:n]
             else:
-                self.__components = list(args[1])
+                self.__components = components_list + [0] * (n - len(components_list))
+
         else:
             raise TypeError("Unsupported arguments type")
 
@@ -38,55 +55,76 @@ class Vector:
     def size(self):
         return len(self.__components)
 
-    def __add__(self, other):
+    def __iadd__(self, other):
+        if not isinstance(other, Vector):
+            raise TypeError("The second value must be Vector")
+
         max_vector = self if self.size >= other.size else other
         min_vector = self if self.size < other.size else other
-        result = list(max_vector.__components)
+        result = Vector(max_vector.__components)
 
-        for i, item in enumerate(min_vector.__components):
+        for i, item in enumerate(min_vector):
             result[i] += item
 
-        return Vector(result)
+        return result
+
+    def __add__(self, other):
+        return Vector(self).__iadd__(other)
+
+    def __isub__(self, other):
+        if not isinstance(other, Vector):
+            raise TypeError("The second value must be Vector")
+
+        result = Vector(other.size, self.__components) if self.size <= other.size else self
+        subtrahend = other if self.size <= other.size else Vector(self.size, other.__components)
+
+        for i, item in enumerate(subtrahend):
+            result[i] -= item
+
+        return result
 
     def __sub__(self, other):
-        other_copy = Vector(other)
-        other_copy.turn()
-        return self + other_copy
+        return Vector(self).__isub__(other)
 
     def __mul__(self, number):
+        if not isinstance(number, (int, float)):
+            raise TypeError("The second value must be number")
+
         self_copy = Vector(self)
         self_copy.__components = [x * number for x in self_copy.__components]
-
         return Vector(self_copy)
 
     __rmul__ = __mul__
 
     def turn(self):
-        self.__components = [-x for x in self.__components]
+        for i in range(self.size):
+            self[i] *= -1
 
-    def dot_product(self, other):
+    def get_dot_product(self, other):
         if not isinstance(other, Vector):
             raise TypeError("The second value must be Vector")
 
         return sum(item_1 * item_2 for item_1, item_2 in zip(self.__components, other.__components))
 
-    def __getitem__(self, item):
-        if not isinstance(item, int):
-            raise TypeError("Item must be int")
+    def __getitem__(self, index):
+        if not isinstance(index, int):
+            raise TypeError("Index must be int")
 
-        if len(self.__components) <= item:
-            raise IndexError("Vector index out of range")
+        if index >= len(self.__components) or index < -len(self.__components):
+            raise IndexError(f"Vector index out of range. Index is {index}, "
+                             f"index must be in [{-self.size}, {self.size})")
 
-        return self.__components[item]
+        return self.__components[index]
 
-    def __setitem__(self, key, value):
-        if not isinstance(key, int):
-            raise TypeError("Key must be int")
+    def __setitem__(self, index, value):
+        if not isinstance(index, int):
+            raise TypeError("Index must be int")
 
-        if len(self.__components) <= key:
-            raise IndexError("Vector index out of range")
+        if index >= len(self.__components) or index < -len(self.__components):
+            raise IndexError(f"Vector index out of range. Index is {index}, "
+                             f"index must be in [{-self.size}, {self.size})")
 
-        self.__components[key] = value
+        self.__components[index] = value
 
     def __eq__(self, other):
         if not isinstance(other, type(self)):

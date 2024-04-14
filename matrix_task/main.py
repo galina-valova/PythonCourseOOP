1665bf0e510ebffacc30a2914c5add0a8a534398
Galina Valova <galinayankova2703@gmail.com>
2024-04-15 00:43:49 +0700

    Fixed issues for Matrix class

--- matrix_task/main.py
+++ matrix_task/main.py
@@ -1,21 +1,19 @@
 from matrix_task.matrix import Matrix
 from vector_task.vector import Vector
 
-matrix = Matrix(2, 2)
-matrix_1 = Matrix(matrix)
-matrix_2 = Matrix([[1, 2, 4], [3, 5, 7], [7, 8, 9]])
-matrix_3 = Matrix([Vector([3, 4, 5]), Vector([6, 8, 11]), Vector([8, 59, 0])])
+matrix_1 = Matrix(2, 2)
+matrix_2 = Matrix([[4, 3], [7, 5]])
+matrix_3 = Matrix([Vector([3, -4, 7]), Vector([6, -8, 0]), Vector([-8, 59])])
+matrix_4 = Matrix(matrix_1)
 
-matrix += Matrix([[5, 9], [11, 10]])
+matrix_4[0] = Vector([2, 5])
+print("matrix_4 ", matrix_4)
 
-for i, row in enumerate([[1, 2], [4, 5]]):
-    matrix_1[i] = row
+matrix_5 = matrix_2 + matrix_4
+print("Sum of two matrices:", matrix_5)
 
-if matrix_2.get_determinant() != 0:
-    print("Matrix 2 has inverse matrix")
+if matrix_3.get_determinant() > 0:
+    print("Transposed matrix_5: ", matrix_5.transpose())
 
-matrices_product = matrix @ matrix_1
-transposed_matrices_product = matrix_1.get_transpose() @ matrix.get_transpose()
-
-if matrices_product.get_determinant() == transposed_matrices_product.get_determinant():
-    print("The transposed product of the matrices is equal to the product of the transposed matrices")
+matrix_6 = matrix_2 @ matrix_4 * 2
+print("Product of matrix_2 and matrix_4 = ", matrix_6)
--- matrix_task/matrix.py
+++ matrix_task/matrix.py
@@ -3,167 +3,208 @@ from vector_task.vector import Vector
 
 class Matrix:
     def __init__(self, *args):
-        if len(args) == 2 and isinstance(args[0], int) and isinstance(args[1], int):
-            n_rows = args[0]
-            n_cols = args[1]
+        arguments_length = len(args)
 
-            if n_rows > 0 and n_cols > 0:
-                self.__rows = [Vector(n_cols)] * n_rows
+        if arguments_length == 2 and isinstance(args[0], int) and isinstance(args[1], int):
+            rows_number = args[0]
+            columns_number = args[1]
+
+            if rows_number < 0 and columns_number < 0:
+                raise ValueError("Matrix dimensions must be positive integer, not", rows_number, columns_number)
+
+            self.__rows = [Vector(columns_number) for _ in range(rows_number)]
+
+        elif arguments_length == 1 and isinstance(args[0], Matrix):
+            self.__rows = [Vector(vector) for vector in args[0].__rows]
+
+        elif arguments_length == 1 and isinstance(args[0], list):
+            rows_list = args[0]
+
+            if all(isinstance(row, list) for row in rows_list):
+                if sum([len(row) for row in rows_list]) > 0:
+                    columns_number = len(max(rows_list, key=len))
+                    self.__rows = [Vector(columns_number, row) for row in rows_list]
+                else:
+                    raise ValueError("List must contains at least one non-empty list")
+
+            elif all(isinstance(row, Vector) for row in rows_list):
+                if len(rows_list) > 0:
+                    columns_number = max(rows_list, key=lambda x: x.size).size
+                    self.__rows = [Vector(columns_number, list(row)) for row in rows_list]
+                else:
+                    raise ValueError("List of vectors must be non-empty")
+
             else:
-                raise ValueError("Matrix dimensions must be positive integer")
-
-        elif len(args) == 1 and isinstance(args[0], Matrix):
-            self.__rows = list(args[0].__rows)
-
-        elif len(args) == 1 and isinstance(args[0], list):
-            components_list = args[0]
-
-            if all(isinstance(item, list) for item in components_list) \
-                    and sum([len(item) for item in components_list]) > 0:
-                n_cols = len(max(components_list, key=len))
-                self.__rows = [Vector(n_cols, item) for item in components_list]
-
-            elif all(isinstance(item, Vector) for item in components_list) \
-                    and sum([item.size for item in components_list]) > 0:
-                n_cols = max(components_list, key=lambda x: x.size).size
-                self.__rows = [Vector(n_cols, list(item)) for item in components_list]
-            else:
-                raise ValueError("Lists or vectors must be not empty")
+                raise ValueError("Unsupported type of list elements")
 
         else:
             raise TypeError("Unsupported arguments type")
 
     @property
-    def size(self):
-        n_rows = len(self.__rows)
-        n_cols = max(self.__rows, key=lambda x: x.size).size
-        return n_rows, n_cols
+    def rows_number(self):
+        return len(self.__rows)
+
+    @property
+    def columns_number(self):
+        return self.__rows[0].size
+
+    def __is_equal_sizes(self, other):
+        return self.rows_number == other.rows_number and self.columns_number == other.columns_number
+
+    def __imul__(self, other):
+        if not isinstance(other, (int, float)):
+            raise TypeError("The second value must be number, not ", type(other))
+
+        for item in self.__rows:
+            item *= other
+
+        return self
 
     def __mul__(self, other):
         if isinstance(other, (int, float)):
-            self_copy = Matrix(self)
-            self_copy.__rows = [row * other for row in self_copy.__rows]
-            return Matrix(self_copy)
+            result = Matrix(self)
+            result *= other
+            return result
 
         if isinstance(other, Vector):
-            if self.size[1] != other.size:
-                raise ValueError("Incompatible matrix and vector sizes for multiplication")
+            if self.columns_number != other.size:
+                raise ValueError(f"Incompatible matrix and vector sizes for multiplication."
+                                 f" Matrix row length {self.columns_number} is not equal to vector length {other.size}")
 
             return Vector([row.get_dot_product(other) for row in self.__rows])
 
-        raise TypeError("The second value must be number")
+        raise TypeError("The second value must be number or Vector, not ", type(other))
 
     __rmul__ = __mul__
 
     def __iadd__(self, other):
         if not isinstance(other, Matrix):
-            raise TypeError("The second value must be Matrix")
+            raise TypeError("The second value must be Matrix, not ", type(other))
 
-        if self.size != other.size:
-            raise ValueError("Incompatible matrix sizes for addition")
+        if not self.__is_equal_sizes(other):
+            raise ValueError(f"Incompatible matrix sizes for addition, your input is"
+                             f"{self.rows_number} x {self.columns_number} and"
+                             f"{other.rows_number} x {other.columns_number}")
 
-        self.__rows = [row_1 + row_2 for row_1, row_2 in zip(self.__rows, other.__rows)]
+        for i in range(self.rows_number):
+            self.__rows[i] += other.__rows[i]
+
         return self
 
     def __add__(self, other):
-        return Matrix(self).__iadd__(other)
+        result = Matrix(self)
+        result += other
+        return result
 
     def __isub__(self, other):
         if not isinstance(other, Matrix):
-            raise TypeError("The second value must be Matrix")
+            raise TypeError("The second value must be Matrix, not ", type(other))
 
-        if self.size != other.size:
-            raise ValueError("Incompatible matrix sizes for subtraction")
+        if not self.__is_equal_sizes(other):
+            raise ValueError(f"Incompatible matrix sizes for subtraction, your input is"
+                             f"{self.rows_number} x {self.columns_number} and"
+                             f"{other.rows_number} x {other.columns_number}")
 
-        self.__rows = [row_1 - row_2 for row_1, row_2 in zip(self.__rows, other.__rows)]
+        for i in range(self.rows_number):
+            self.__rows[i] -= other.__rows[i]
+
         return self
 
     def __sub__(self, other):
-        return Matrix(self).__isub__(other)
+        result = Matrix(self)
+        result -= other
+        return result
 
     def __matmul__(self, other):
         if not isinstance(other, Matrix):
-            raise TypeError("The second value must be Matrix")
+            raise TypeError("The second value must be Matrix, not ", type(other))
 
-        if self.size[1] != other.size[0]:
-            raise ValueError("Incompatible matrix sizes for multiplication")
+        if self.columns_number != other.rows_number:
+            raise ValueError(f"Incompatible matrix sizes for multiplication: number of columns {self.columns_number}"
+                             f" is not equal to number of rows {other.rows_number}")
 
-        return Matrix([[row.get_dot_product(Vector(list(col))) for col in zip(*other.__rows)] for row in self.__rows])
+        transposed_other = Matrix(other).transpose()
+        return Matrix([[row_1.get_dot_product(row_2) for row_2 in transposed_other.__rows] for row_1 in self.__rows])
 
-    def get_transpose(self):
+    def transpose(self):
         self.__rows = [Vector(list(item)) for item in (zip(*self.__rows))]
         return self
 
     def __setitem__(self, index, value):
         if not isinstance(index, int):
-            raise TypeError("Key must be int")
+            raise TypeError("Index must be int, not ", type(index))
 
-        if index >= self.size[0] or index < -self.size[0]:
+        if not isinstance(value, (list, Vector)):
+            raise TypeError("Value must be list or Vector, not ", type(index))
+
+        if index >= self.rows_number or index < -self.rows_number:
             raise IndexError(f"Row index out of range. Index is {index}, "
-                             f"index must be in [{-self.size[0]}, {self.size[0]})")
+                             f"index must be in [{-self.rows_number}, {self.rows_number})")
 
-        if isinstance(value, list) and self.size[1] != len(value):
-            raise ValueError("Incompatible row length")
+        if isinstance(value, list) and self.columns_number != len(value):
+            raise ValueError(f"Incompatible rows length: matrix row length {self.columns_number} is not equal to"
+                             f" row-vector length {len(value)}")
 
-        elif isinstance(value, Vector) and self.size[1] != value.size:
-            raise ValueError("Incompatible row length")
+        elif isinstance(value, Vector) and self.columns_number != value.size:
+            raise ValueError(f"Incompatible rows length: matrix row length {self.columns_number} is not equal to"
+                             f" row-vector length {value.size}")
 
         self.__rows[index] = Vector(value)
 
     def __getitem__(self, index):
         if not isinstance(index, int):
-            raise TypeError("Item must be int")
+            raise TypeError("Index must be int, not ", type(index))
 
-        if index >= self.size[0] or index < -self.size[0]:
+        if index >= self.rows_number or index < -self.rows_number:
             raise IndexError(f"Row index out of range. Index is {index}, "
-                             f"index must be in [{-self.size[0]}, {self.size[0]})")
+                             f"index must be in [{-self.rows_number}, {self.rows_number})")
 
-        return self.__rows[index]
+        return Vector(self.__rows[index])
 
-    def get_col(self, index):
+    def get_column(self, index):
         if not isinstance(index, int):
-            raise TypeError("Item must be int")
+            raise TypeError("Item must be int, not ", type(index))
 
-        if index >= self.size[1] or index < -self.size[1]:
+        if index >= self.columns_number or index < -self.columns_number:
             raise IndexError(f"Column index out of range. Index is {index}, "
-                             f"index must be in [{-self.size[1]}, {self.size[1]})")
+                             f"index must be in [{-self.columns_number}, {self.columns_number})")
 
         return Vector([row[index] for row in self.__rows])
 
-    def get_algebraic_complement(self, row_item_index, col_item_index):
-        if row_item_index >= self.size[0] or col_item_index >= self.size[1]:
+    def __get_algebraic_complement(self, row_item_index, col_item_index):
+        if row_item_index >= self.rows_number or col_item_index >= self.rows_number:
             raise IndexError("Indexes out of range")
 
-        if self.size[0] < 2 or self.size[1] < 2:
+        if self.rows_number < 2 or self.columns_number < 2:
             raise TypeError("Requires a matrix, not a vector")
 
         matrix_elements = []
 
-        for i in range(self.size[0]):
-            for j in range(self.size[1]):
+        for i in range(self.rows_number):
+            for j in range(self.columns_number):
 
                 if i != row_item_index:
                     if j != col_item_index:
                         matrix_elements.append(self.__rows[i][j])
 
-        return Matrix([Vector(matrix_elements[i:i + (self.size[0] - 1)]) for i in range(0, len(matrix_elements),
-                                                                                        (self.size[0] - 1))])
+        return Matrix([Vector(matrix_elements[i:i + (self.rows_number - 1)]) for i in range(0, len(matrix_elements),
+                                                                                            (self.rows_number - 1))])
 
     def get_determinant(self):
-        if self.size[0] != self.size[1]:
-            raise TypeError("Incompatible matrix for calculate determinant")
+        if self.rows_number != self.columns_number:
+            raise TypeError(f"Matrix must be square. Your matrix has size {self.rows_number} x {self.columns_number}")
 
-        if self.size == (1, 1):
+        if self.rows_number == 1 and self.columns_number == 1:
             return self[0][0]
 
-        if self.size == (2, 2):
+        if self.rows_number == 2 and self.columns_number == 2:
             return self[0][0] * self[1][1] - self[0][1] * self[1][0]
 
         else:
             determinant = 0
 
-            for j in range(self.size[0]):
-                determinant += (-1) ** j * self.__rows[0][j] * self.get_algebraic_complement(0, j).get_determinant()
+            for i in range(self.rows_number):
+                determinant += (-1) ** i * self.__rows[0][i] * self.__get_algebraic_complement(0, i).get_determinant()
 
             return determinant
 
